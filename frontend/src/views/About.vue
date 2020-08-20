43c1b26da8c6ce2158175f181e30616de4aa4bff
jny9708 <jny9708@gmail.com>
2020-08-20 19:14:30 +0900

    custom toolbar, getcontents test

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-    <quill-editor></quill-editor>
+  <div>
+      <quill-editor 
+      v-model="quillContent"
+      :options="editorOption"
+      ref="myQuillEditor"
+      >
+        <!-- <quill-editor > -->
+        <div id="toolbar" slot="toolbar">
+          <span class="ql-formats">
+            <select class="ql-size">
+              <option value="small"></option>
+              <option selected></option>
+              <option value="large"></option>
+              <option value="huge"></option>
+            </select>
+          </span>
+          <span class="ql-formats">          
+            <button class="ql-bold"></button>
+            <button class="ql-italic"></button>
+            <button class="ql-underline"></button>
+            <button class="ql-link"></button>
+          </span>
+          <span class="ql-formats">
+            <button class="ql-list" value="ordered"></button>
+            <button class="ql-list" value="bullet"></button>
+          </span>
+
+          <span class="ql-formats">
+            <button class="ql-clean"></button>
+          </span>
+        </div>
+      </quill-editor>
+
+      <button @click = "click">click</button>
+      <div class="ql-editor" v-html="quillContent">
+
+      </div>
+  </div>
 </template>
 
 <script>
@@ -18,15 +55,28 @@ import { quillEditor } from 'vue-quill-editor'
     computed: {},
     data() {
       return {
-       
+       quillContent:'',
+       editorOption: {
+          modules: {
+            toolbar: '#toolbar',
+            markdownShortcuts: {}
+          }
+        }
       };
     },
     methods: {
-
+      click:function(){
+        console.log(this.quillContent)
+        
+        // var delta = .getContents();
+        // console.log(delta);
+      }
     }
   }
 
 </script>
 <style scoped>
-
+.ql-custom-button {
+        width: 100px;
+      }
 </style>
